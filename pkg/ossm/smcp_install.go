adf18f5e87c772aa3cb8952f0cca7d46138a7ef6
Pritam Kharat <pritam.kharat@oneconvergence.com>
2024-10-09 07:32:17 +0000

    Update maistra-2.3 branch to
    1) Work with new smcp versions 2.5,2.4,2.3
    2) Change nginx spec image pull policy to IfNotPresent
    3) Change istio-ingressgateway svc type from ClusterIP to
       LoadBalancer at the end of test A1

--- pkg/ossm/smcp_install.go
+++ pkg/ossm/smcp_install.go
@@ -16,16 +16,18 @@ package ossm
 
 import (
 	"strings"
+	"fmt"
 	"testing"
 	"time"
 
+        "os"
 	"github.com/maistra/maistra-test-tool/pkg/util"
 )
 
-func installDefaultSMCP23() {
-	util.Log.Info("Create SMCP v2.3 in ", meshNamespace)
+func installDefaultSMCP25() {
+	util.Log.Info("Create SMCP v2.5 in ", meshNamespace)
 	util.ShellMuteOutputError(`oc new-project %s`, meshNamespace)
-	util.KubeApplyContents(meshNamespace, util.RunTemplate(smcpV23_template, smcp))
+	util.KubeApplyContents(meshNamespace, util.RunTemplate(smcpV25_template, smcp))
 	util.KubeApplyContents(meshNamespace, smmr)
 
 	// patch SMCP identity if it's on a ROSA cluster
@@ -37,10 +39,10 @@ func installDefaultSMCP23() {
 	time.Sleep(time.Duration(20) * time.Second)
 }
 
-func installDefaultSMCP22() {
-	util.Log.Info("Create SMCP v2.2 in ", meshNamespace)
+func installDefaultSMCP24() {
+	util.Log.Info("Create SMCP v2.4 in ", meshNamespace)
 	util.ShellMuteOutputError(`oc new-project %s`, meshNamespace)
-	util.KubeApplyContents(meshNamespace, util.RunTemplate(smcpV22_template, smcp))
+	util.KubeApplyContents(meshNamespace, util.RunTemplate(smcpV24_template, smcp))
 	util.KubeApplyContents(meshNamespace, smmr)
 
 	// patch SMCP identity if it's on a ROSA cluster
@@ -50,14 +52,121 @@ func installDefaultSMCP22() {
 	util.Log.Info("Waiting for mesh installation to complete")
 	util.Shell(`oc wait --for condition=Ready -n %s smcp/%s --timeout 300s`, meshNamespace, smcpName)
 	time.Sleep(time.Duration(20) * time.Second)
+}
+
+func changeSvcToLB() {
+    util.Log.Info("Changing service type from ClusterIP to LoadBalancer")
+    time.Sleep(time.Duration(20) * time.Second)
+
+    // Change service type to LoadBalancer
+    util.Shell(`oc patch svc istio-ingressgateway -n istio-system -p '{"spec": {"type": "LoadBalancer"}}'`)
+
+    // Get the HOST NAME
+    host_name, err := util.Shell(`oc get route -n istio-system istio-ingressgateway -o jsonpath='{.spec.host}'`)
+    util.Log.Info("Retrieved Host Name of route istio-ingressgateway: ", host_name)
+    if err != nil {
+        util.Log.Error("Failed to get Host Name of route istio-ingressgateway: ", err)
+        os.Exit(1)
+    }
+
+    // Get the EXTERNAL IP
+    external_ip, err := util.Shell(`oc get svc -n istio-system istio-ingressgateway -o jsonpath='{.status.loadBalancer.ingress[0].ip}'`)
+    util.Log.Info("Retrieved External IP of svc istio-ingressgateway: ", external_ip)
+    if err != nil {
+        util.Log.Error("Failed to get External IP of svc istio-ingressgateway: ", err)
+        os.Exit(1)
+    }
+
+    // Update /etc/hosts
+    util.Log.Info("Updating /etc/hosts file with External IP and Host Name entry for DNS resolution")
+    existingEntry, _ := util.Shell(fmt.Sprintf(`grep "%s" /etc/hosts`, host_name))
+
+    if existingEntry == "" {
+        // If the hostname doesn't exist, append it
+        _, err := util.Shell(fmt.Sprintf(`echo "%s %s" | sudo tee -a /etc/hosts`, external_ip, host_name))
+        if err != nil {
+            util.Log.Error("Failed to append /etc/hosts file with External IP and Host Name entry: ", err)
+	    os.Exit(1)
+	}
+    } else {
+        // If it exists, update the line
+        _, err := util.Shell(fmt.Sprintf(`sudo sed -i 's/.*%s$/%s %s/' /etc/hosts`, host_name, external_ip, host_name))
+        if err != nil {
+            util.Log.Error("Failed to update /etc/hosts file with External IP and Host Name entry ", err)
+	    os.Exit(1)
+	}
+    }
 }
 
 func TestSMCPInstall(t *testing.T) {
-	defer installDefaultSMCP23()
+	defer changeSvcToLB()
+	defer installDefaultSMCP25()
+
+	t.Run("smcp_test_install_2.5", func(t *testing.T) {
+		defer util.RecoverPanic(t)
+		util.Log.Info("Create SMCP v2.5 in ", meshNamespace)
+		util.ShellMuteOutputError(`oc new-project %s`, meshNamespace)
+		util.KubeApplyContents(meshNamespace, util.RunTemplate(smcpV25_template, smcp))
+		util.KubeApplyContents(meshNamespace, smmr)
+
+		// patch SMCP identity if it's on a ROSA cluster
+		if util.Getenv("ROSA", "false") == "true" {
+			util.Shell(`oc patch -n %s smcp/%s --type merge -p '{"spec":{"security":{"identity":{"type":"ThirdParty"}}}}'`, meshNamespace, smcpName)
+		}
+		util.Log.Info("Waiting for mesh installation to complete")
+		util.Shell(`oc wait --for condition=Ready -n %s smcp/%s --timeout 300s`, meshNamespace, smcpName)
+
+		util.Log.Info("Verify SMCP status and pods")
+		msg, _ := util.Shell(`oc get -n %s smcp/%s -o wide`, meshNamespace, smcpName)
+		if !strings.Contains(msg, "ComponentsReady") {
+			util.Log.Error("SMCP not Ready")
+			t.Error("SMCP not Ready")
+		}
+		util.Shell(`oc get -n %s pods`, meshNamespace)
+	})
+
+	t.Run("smcp_test_uninstall_2.5", func(t *testing.T) {
+		defer util.RecoverPanic(t)
+		util.Log.Info("Delete SMCP v2.5 in ", meshNamespace)
+		util.KubeDeleteContents(meshNamespace, smmr)
+		util.KubeDeleteContents(meshNamespace, util.RunTemplate(smcpV25_template, smcp))
+		time.Sleep(time.Duration(40) * time.Second)
+	})
+
+	t.Run("smcp_test_install_2.4", func(t *testing.T) {
+		defer util.RecoverPanic(t)
+		util.Log.Info("Create SMCP v2.4 in ", meshNamespace)
+		util.ShellMuteOutputError(`oc new-project %s`, meshNamespace)
+		util.KubeApplyContents(meshNamespace, util.RunTemplate(smcpV24_template, smcp))
+		util.KubeApplyContents(meshNamespace, smmr)
+
+		// patch SMCP identity if it's on a ROSA cluster
+		if util.Getenv("ROSA", "false") == "true" {
+			util.Shell(`oc patch -n %s smcp/%s --type merge -p '{"spec":{"security":{"identity":{"type":"ThirdParty"}}}}'`, meshNamespace, smcpName)
+		}
+		util.Log.Info("Waiting for mesh installation to complete")
+		util.Shell(`oc wait --for condition=Ready -n %s smcp/%s --timeout 300s`, meshNamespace, smcpName)
+
+		util.Log.Info("Verify SMCP status and pods")
+		msg, _ := util.Shell(`oc get -n %s smcp/%s -o wide`, meshNamespace, smcpName)
+		if !strings.Contains(msg, "ComponentsReady") {
+			util.Log.Error("SMCP not Ready")
+			t.Error("SMCP not Ready")
+		}
+		util.Shell(`oc get -n %s pods`, meshNamespace)
+	})
+
+	t.Run("smcp_test_uninstall_2.4", func(t *testing.T) {
+		defer util.RecoverPanic(t)
+		util.Log.Info("Delete SMCP v2.4 in ", meshNamespace)
+		util.KubeDeleteContents(meshNamespace, smmr)
+		util.KubeDeleteContents(meshNamespace, util.RunTemplate(smcpV24_template, smcp))
+		time.Sleep(time.Duration(40) * time.Second)
+	})
 
 	t.Run("smcp_test_install_2.3", func(t *testing.T) {
 		defer util.RecoverPanic(t)
-		util.Log.Info("Create SMCP v2.3 in ", meshNamespace)
+		util.Log.Info("Create SMCP v2.3 in namespace ", meshNamespace)
 		util.ShellMuteOutputError(`oc new-project %s`, meshNamespace)
 		util.KubeApplyContents(meshNamespace, util.RunTemplate(smcpV23_template, smcp))
 		util.KubeApplyContents(meshNamespace, smmr)
@@ -83,77 +192,15 @@ func TestSMCPInstall(t *testing.T) {
 		util.Log.Info("Delete SMCP v2.3 in ", meshNamespace)
 		util.KubeDeleteContents(meshNamespace, smmr)
 		util.KubeDeleteContents(meshNamespace, util.RunTemplate(smcpV23_template, smcp))
-		time.Sleep(time.Duration(40) * time.Second)
-	})
-
-	t.Run("smcp_test_install_2.2", func(t *testing.T) {
-		defer util.RecoverPanic(t)
-		util.Log.Info("Create SMCP v2.2 in ", meshNamespace)
-		util.ShellMuteOutputError(`oc new-project %s`, meshNamespace)
-		util.KubeApplyContents(meshNamespace, util.RunTemplate(smcpV22_template, smcp))
-		util.KubeApplyContents(meshNamespace, smmr)
-
-		// patch SMCP identity if it's on a ROSA cluster
-		if util.Getenv("ROSA", "false") == "true" {
-			util.Shell(`oc patch -n %s smcp/%s --type merge -p '{"spec":{"security":{"identity":{"type":"ThirdParty"}}}}'`, meshNamespace, smcpName)
-		}
-		util.Log.Info("Waiting for mesh installation to complete")
-		util.Shell(`oc wait --for condition=Ready -n %s smcp/%s --timeout 300s`, meshNamespace, smcpName)
-
-		util.Log.Info("Verify SMCP status and pods")
-		msg, _ := util.Shell(`oc get -n %s smcp/%s -o wide`, meshNamespace, smcpName)
-		if !strings.Contains(msg, "ComponentsReady") {
-			util.Log.Error("SMCP not Ready")
-			t.Error("SMCP not Ready")
-		}
-		util.Shell(`oc get -n %s pods`, meshNamespace)
-	})
-
-	t.Run("smcp_test_uninstall_2.2", func(t *testing.T) {
-		defer util.RecoverPanic(t)
-		util.Log.Info("Delete SMCP v2.2 in ", meshNamespace)
-		util.KubeDeleteContents(meshNamespace, smmr)
-		util.KubeDeleteContents(meshNamespace, util.RunTemplate(smcpV22_template, smcp))
-		time.Sleep(time.Duration(40) * time.Second)
-	})
-
-	t.Run("smcp_test_install_2.1", func(t *testing.T) {
-		defer util.RecoverPanic(t)
-		util.Log.Info("Create SMCP v2.1 in namespace ", meshNamespace)
-		util.ShellMuteOutputError(`oc new-project %s`, meshNamespace)
-		util.KubeApplyContents(meshNamespace, util.RunTemplate(smcpV21_template, smcp))
-		util.KubeApplyContents(meshNamespace, smmr)
-
-		// patch SMCP identity if it's on a ROSA cluster
-		if util.Getenv("ROSA", "false") == "true" {
-			util.Shell(`oc patch -n %s smcp/%s --type merge -p '{"spec":{"security":{"identity":{"type":"ThirdParty"}}}}'`, meshNamespace, smcpName)
-		}
-		util.Log.Info("Waiting for mesh installation to complete")
-		util.Shell(`oc wait --for condition=Ready -n %s smcp/%s --timeout 300s`, meshNamespace, smcpName)
-
-		util.Log.Info("Verify SMCP status and pods")
-		msg, _ := util.Shell(`oc get -n %s smcp/%s -o wide`, meshNamespace, smcpName)
-		if !strings.Contains(msg, "ComponentsReady") {
-			util.Log.Error("SMCP not Ready")
-			t.Error("SMCP not Ready")
-		}
-		util.Shell(`oc get -n %s pods`, meshNamespace)
-	})
-
-	t.Run("smcp_test_uninstall_2.1", func(t *testing.T) {
-		defer util.RecoverPanic(t)
-		util.Log.Info("Delete SMCP v2.1 in ", meshNamespace)
-		util.KubeDeleteContents(meshNamespace, smmr)
-		util.KubeDeleteContents(meshNamespace, util.RunTemplate(smcpV21_template, smcp))
 		time.Sleep(time.Duration(60) * time.Second)
 	})
 
-	t.Run("smcp_test_upgrade_2.1_to_2.2", func(t *testing.T) {
+	t.Run("smcp_test_upgrade_2.3_to_2.4", func(t *testing.T) {
 		defer util.RecoverPanic(t)
 
-		util.Log.Info("Create SMCP v2.1 in namespace ", meshNamespace)
+		util.Log.Info("Create SMCP v2.3 in namespace ", meshNamespace)
 		util.ShellMuteOutputError(`oc new-project %s`, meshNamespace)
-		util.KubeApplyContents(meshNamespace, util.RunTemplate(smcpV21_template, smcp))
+		util.KubeApplyContents(meshNamespace, util.RunTemplate(smcpV23_template, smcp))
 		util.KubeApplyContents(meshNamespace, smmr)
 
 		// patch SMCP identity if it's on a ROSA cluster
@@ -167,8 +214,8 @@ func TestSMCPInstall(t *testing.T) {
 		util.Shell(`oc get -n %s smcp/%s -o wide`, meshNamespace, smcpName)
 		util.Shell(`oc get -n %s pods`, meshNamespace)
 
-		util.Log.Info("Upgrade SMCP to v2.2")
-		util.KubeApplyContents(meshNamespace, util.RunTemplate(smcpV22_template, smcp))
+		util.Log.Info("Upgrade SMCP to v2.4")
+		util.KubeApplyContents(meshNamespace, util.RunTemplate(smcpV24_template, smcp))
 
 		// patch SMCP identity if it's on a ROSA cluster
 		if util.Getenv("ROSA", "false") == "true" {
@@ -187,12 +234,12 @@ func TestSMCPInstall(t *testing.T) {
 		util.Shell(`oc get -n %s pods`, meshNamespace)
 	})
 
-	t.Run("smcp_test_upgrade_2.2_to_2.3", func(t *testing.T) {
+	t.Run("smcp_test_upgrade_2.4_to_2.5", func(t *testing.T) {
 		defer util.RecoverPanic(t)
 
-		util.Log.Info("Create SMCP v2.2 in namespace ", meshNamespace)
+		util.Log.Info("Create SMCP v2.4 in namespace ", meshNamespace)
 		util.ShellMuteOutputError(`oc new-project %s`, meshNamespace)
-		util.KubeApplyContents(meshNamespace, util.RunTemplate(smcpV22_template, smcp))
+		util.KubeApplyContents(meshNamespace, util.RunTemplate(smcpV24_template, smcp))
 		util.KubeApplyContents(meshNamespace, smmr)
 
 		// patch SMCP identity if it's on a ROSA cluster
@@ -206,8 +253,8 @@ func TestSMCPInstall(t *testing.T) {
 		util.Shell(`oc get -n %s smcp/%s -o wide`, meshNamespace, smcpName)
 		util.Shell(`oc get -n %s pods`, meshNamespace)
 
-		util.Log.Info("Upgrade SMCP to v2.2")
-		util.KubeApplyContents(meshNamespace, util.RunTemplate(smcpV23_template, smcp))
+		util.Log.Info("Upgrade SMCP to v2.5")
+		util.KubeApplyContents(meshNamespace, util.RunTemplate(smcpV25_template, smcp))
 
 		// patch SMCP identity if it's on a ROSA cluster
 		if util.Getenv("ROSA", "false") == "true" {
